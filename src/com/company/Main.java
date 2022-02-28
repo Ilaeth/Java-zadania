9b4f65ec6b9800a50768bf9d759bfd691fa02fde
Ilaeth <przemek1999.10.5@wp.pl>
2022-02-28 17:26:57 +0100

    Zadanie 2 - Get yourself a car

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,21 +7,10 @@ public class Main {
 
         Animal cat = new Animal("cat", "Jinx");
 
-        Human me = new Human("Przemek", "Podlecki", "123456789", cat);
+        Car bmw = new Car("x3", "bmw");
+
+        Human me = new Human("Przemek", "Podlecki", "123456789", cat, bmw);
 
         Animal dog = new Animal("dog", "Hawana");
-
-        System.out.println(dog.name);
-        dog.feed();
-        dog.feed();
-        dog.takeForAWalk();
-
-        System.out.println(cat.name);
-        cat.takeForAWalk();
-        cat.takeForAWalk();
-        cat.takeForAWalk();
-        cat.takeForAWalk();
-        cat.takeForAWalk();
-        cat.takeForAWalk();
     }
 }
