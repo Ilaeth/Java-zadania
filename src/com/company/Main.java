6642541b7c2e5883e88f77f0a4da79998a6801f2
Ilaeth <przemek1999.10.5@wp.pl>
2022-02-28 13:14:44 +0100

    Zadanie 0 - wstępna konfiguracja

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,25 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Animal dog = new Animal();
+        dog.species = "dog";
+        dog.name = "Hawana";
+        dog.weight = 50.0;
+
+        Animal cat = new Animal();
+        cat.species = "cat";
+        cat.name = "Jinx";
+        cat.weight = 5.0;
+
+        Phone samsung = new Phone();
+        samsung.screenSize = 6.1;
+        samsung.producer = "Samsung";
+        samsung.model = "S10";
+        samsung.operationSystem = "android";
+
+        Human me = new Human();
+        me.firstName = "Przemek";
+        me.lastName = "Podlecki";
+        me.phone = "Samsung S10";
     }
 }
