4f128bbe7a6b617062e83f5f19ae06cc7745717f
Ilaeth <przemek1999.10.5@wp.pl>
2022-02-28 18:51:45 +0100

    Zadanie 5 - Check if you could buy it

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,16 +7,22 @@ public class Main {
 
         Animal cat = new Animal("cat", "Jinx");
 
-        Car bmw = new Car("x3", "bmw");
+        Car bmw = new Car("x3", "bmw", 45000.00);
+
+        Car bmw2 = new Car("5", "bmw", 100000.00);
+
+        Car audi = new Car("4", "audi", 700000.00);
 
         Human me = new Human("Przemek", "Podlecki", "123456789", cat, bmw);
 
         Animal dog = new Animal("dog", "Hawana");
 
-        me.getterSalary();
-        me.setterSalary(-50000);
-        me.getterSalary();
-        me.setterSalary(50000);
-        me.getterSalary();
+        me.setterSalary(50000.00);
+
+        me.setterCar(bmw);
+        System.out.println(me.getterCar());
+        me.setterCar(bmw2);
+        System.out.println(me.getterCar());
+        me.setterCar(audi);
     }
 }
